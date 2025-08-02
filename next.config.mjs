58f6e8a7d6d448cdf83b926bf5061a095cc47813
adekoyesuccess <successadekoye94@gmail.com>
2025-08-02 11:39:51 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ process.env.NEXT_DISABLE_STRICT_PNPM_CHECK = 'true';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: process.env.NODE_ENV !== 'development',
 
   env: {
